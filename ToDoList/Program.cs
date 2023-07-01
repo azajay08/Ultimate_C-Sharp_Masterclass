584b8d2df52a591f46e7430e4c93d32e753936a4
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-07-01 21:41:08 +0300

    tried breakpoints and input

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
 
+String userChoice = Console.ReadLine();
+Console.WriteLine("User Input: " + userChoice);
+userChoice = "ABC";
+var reuslt = "abc" + "def" + "ghi";
+Console.ReadKey();
