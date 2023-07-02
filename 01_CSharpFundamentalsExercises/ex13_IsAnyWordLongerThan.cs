0da3357a51d3dd0b0b41d4c22fc8beb6c8d64a1b
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-07-03 00:11:08 +0300

    ex 13 foreach loop

---
+++ 01_CSharpFundamentalsExercises/ex13_IsAnyWordLongerThan.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Coding.Exercise
+{
+	public class Exercise
+	{
+		public static bool IsAnyWordLongerThan(int length, string[] words)
+		{
+			foreach (var word in words)
+			{
+				if (word.Length > length)
+					return true;
+			}
+			return false;
+		}
+	}
+}
