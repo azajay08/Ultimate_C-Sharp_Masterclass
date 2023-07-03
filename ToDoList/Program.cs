d83ccf2c05601b0fa655a7989544efc2bdbf3dac
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-07-03 13:57:22 +0300

    completed the todo list app

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,24 +1,107 @@
-﻿Console.WriteLine("Hello!");
-Console.WriteLine("What do you want to do?");
-Console.WriteLine("[S]ee all TODOs");
-Console.WriteLine("[A]dd a TODO");
-Console.WriteLine("[R]emove a TODO");
-Console.WriteLine("[E]xit");
+﻿using System.Xml;
 
-String userChoice = Console.ReadLine();
+var toTOList = new List<string>();
 
-Console.WriteLine("User Input: " + userChoice);
+while (true)
+{
+	Console.WriteLine();
+	Console.WriteLine("What do you want to do?");
+	Console.WriteLine("[S]ee all todos");
+	Console.WriteLine("[A]dd a todo");
+	Console.WriteLine("[R]emove a todo");
+	Console.WriteLine("[E]xit");
 
-if (userChoice.ToUpper() == "S")
-	PrintSelectedOption("See all TODOs");
-else if (userChoice.ToUpper() == "A")
-	PrintSelectedOption("Add a TODO");
-else if (userChoice.ToUpper() == "R")
-	PrintSelectedOption("Remove a TODO");
-else if (userChoice.ToUpper() == "E")
-	PrintSelectedOption("Exit");
-else
-	Console.WriteLine("Invalid choice!");
+	var userChoice = Console.ReadLine();
+
+	if (userChoice.ToUpper() == "S")
+		showToDo();
+	else if (userChoice.ToUpper() == "A")
+		addToList();
+	else if (userChoice.ToUpper() == "R")
+		removeToDo();
+	else if (userChoice.ToUpper() == "E")
+		break;
+	else
+		Console.WriteLine("Invalid choice!");
+}
+
+void removeToDo()
+{
+	int index;
+	if (toTOList.Count == 0)
+	{
+		Console.WriteLine("No TODOs have been added yet.");
+		return;
+	}
+	while (true)
+	{
+		Console.WriteLine("Select the index of the TODO you want to remove");
+		showToDo();
+		if (inputIsValid(out index))
+		{
+			removeIndex(index);
+			break;
+		}
+	}
+}
+
+void removeIndex(int index)
+{
+	var toDoToBeRemoved = toTOList[index - 1];
+	toTOList.Remove(toDoToBeRemoved);
+	Console.WriteLine("TODO removed: " + toDoToBeRemoved);
+}
+
+bool inputIsValid(out int index)
+{
+	var userIndex = Console.ReadLine();
+	if (userIndex == "")
+	{
+		index = 0;
+		Console.WriteLine("Selected index cannot be empty");
+		return false;
+	}
+	if (int.TryParse(userIndex, out index) && index >= 1 && index <= toTOList.Count)
+		return true;
+	Console.WriteLine("The given index is not valid.");
+	return false;
+}
+
+void addToList()
+{
+	while (true)
+	{
+		Console.WriteLine("Enter the TODO description");
+		var userInput = Console.ReadLine();
+
+		if (toTOList.Contains(userInput))
+			Console.WriteLine("The description must be unique.");
+		else if (userInput == "")
+			Console.WriteLine("The description cannot be empty.");
+		else
+		{
+			toTOList.Add(userInput);
+			break;
+		}
+
+	}
+}
+
+void showToDo()
+{
+	if (toTOList.Count == 0)
+	{
+		Console.WriteLine("No TODOs have been added yet.");
+		return;
+	}
+	for (int i = 0; i < toTOList.Count; i++)
+	{
+		Console.WriteLine($"{i + 1}. {toTOList[i]}");
+	}
+}
+
+
+//Console.ReadKey();
 
 //switch(userChoice)
 //{
@@ -42,13 +125,3 @@ else
 //		Console.WriteLine("Invalid choice!");
 //		break;
 //}
-
-void PrintSelectedOption(String userInput)
-{
-	Console.WriteLine("Selected option: " + userInput);
-}
-
-
-//bool isUserInputAbc = userChoice == "ABC"; // same as asking if its equal to
-
-Console.ReadKey();
