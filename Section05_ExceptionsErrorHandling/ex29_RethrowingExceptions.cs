0db1175c812b13450420fc95629e61f24f706d10
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-07-12 12:27:31 +0300

    ex 29 done

---
+++ Section05_ExceptionsErrorHandling/ex29_RethrowingExceptions.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Coding.Exercise
+{
+	public static class ExceptionsRethrowing
+	{
+		public static int GetMaxValue(List<int> numbers)
+		{
+			try
+			{
+				return numbers.Max();
+			}
+			catch (ArgumentNullException ex)
+			{
+				throw new ArgumentNullException("The numbers list cannot be null.", ex);
+			}
+			catch (InvalidOperationException ex)
+			{
+				Console.WriteLine("The numbers list cannot be empty.");
+				throw;
+			}
+		}
+	}
+}
