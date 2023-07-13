dd1b2d0a0a9f4ef5a3990bd5cd7e597e8cd4330f
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-07-13 23:08:00 +0300

    ex 31 done

---
+++ CodingExercises/Section06_GenericTypes/ex31_PairClass.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Coding.Exercise
+{
+	public class Pair<T>
+	{
+		public T First { get; private set; }
+		public T Second { get; private set; }
+		
+		public Pair(T first, T second)
+		{
+			First = first;
+			Second = second;
+		}
+		
+		public void ResetFirst()
+		{
+			First = default;
+		}
+		
+		public void ResetSecond()
+		{
+			Second = default;   
+		}
+	}
+}
