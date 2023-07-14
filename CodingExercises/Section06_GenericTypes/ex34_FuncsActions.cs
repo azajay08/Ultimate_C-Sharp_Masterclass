ca6b2cb1cf9bfee3bd475006d4cc57035385c4a7
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-07-14 23:42:59 +0300

    ex 34 done

---
+++ CodingExercises/Section06_GenericTypes/ex34_FuncsActions.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Coding.Exercise
+{
+	public class Exercise
+	{
+		public void TestMethod()
+		{
+			Func<int, bool, double> someMethod1 = Method1;
+			Func<DateTime> someMethod2 = Method2;
+			Action<string, string> someMethod3 = Method3;
+		}
+		
+		public double Method1(int a, bool b) => 0;
+		public DateTime Method2() => default(DateTime);
+		public void Method3(string a, string b) {}
+	}
+}
