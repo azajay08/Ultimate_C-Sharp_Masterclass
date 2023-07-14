dda395464555ab7270f6ffb4c37178c728e899e5
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-07-15 00:03:34 +0300

    ex 35 done

---
+++ CodingExercises/Section06_GenericTypes/ex35_BasicLambda.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Coding.Exercise
+{
+	public class Exercise
+	{
+		public Func<string, int> GetLength = str => str.Length;
+		public Func<int> GetRandomNumberBetween1And10 = () => new Random().Next(1, 11);
+	}
+}
