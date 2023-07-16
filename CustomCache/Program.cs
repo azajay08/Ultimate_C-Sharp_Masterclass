b9b098763be7d3c9029f2cdc24f661561bbb3414
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-07-16 23:52:48 +0300

    finished assignment on caching

--- CustomCache/Program.cs
+++ CustomCache/Program.cs
@@ -1,4 +1,5 @@
-﻿IDataDownloader dataDownloader = new SlowDataDownloader();
+﻿IDataDownloader dataDownloader = new CachingDataDownloader(
+	new PrintingDataDownloader(new SlowDataDownloader()));
 
 Console.WriteLine(dataDownloader.DownloadData("id1"));
 Console.WriteLine(dataDownloader.DownloadData("id2"));
@@ -10,6 +11,53 @@ Console.WriteLine(dataDownloader.DownloadData("id2"));
 
 Console.ReadKey();
 
+public class Cache<TKey, TData>
+{
+	private readonly Dictionary<TKey, TData> _cacheData = new();
+
+	public TData Get(TKey key, Func<TKey, TData> newEntry)
+	{
+		if (!_cacheData.ContainsKey(key))
+		{
+			_cacheData[key] = newEntry(key);
+		}
+
+		return _cacheData[key];
+	}
+}
+public class PrintingDataDownloader : IDataDownloader
+{
+	private readonly IDataDownloader _dataDownloader;
+
+	public PrintingDataDownloader(IDataDownloader dataDownloader)
+	{
+		_dataDownloader = dataDownloader;
+	}
+
+	public string DownloadData(string resourceId)
+	{
+		var data = _dataDownloader.DownloadData(resourceId);
+		Console.WriteLine("Data is ready!");
+		return data;
+	}
+}
+
+public class CachingDataDownloader : IDataDownloader
+{
+	private readonly IDataDownloader _dataDownloader;
+	private readonly Cache<string, string> _cache = new();
+
+	public CachingDataDownloader(IDataDownloader dataDownloader)
+	{
+		_dataDownloader = dataDownloader;
+	}
+
+	public string DownloadData(string resourceId)
+	{
+		return _cache.Get(resourceId, _dataDownloader.DownloadData);
+	}
+}
+
 public interface IDataDownloader
 {
 	string DownloadData(string resourceId);
@@ -17,6 +65,7 @@ public interface IDataDownloader
 
 public class SlowDataDownloader : IDataDownloader
 {
+
 	public string DownloadData(string resourceId)
 	{
 		//let's imagine this method downloads real data,
