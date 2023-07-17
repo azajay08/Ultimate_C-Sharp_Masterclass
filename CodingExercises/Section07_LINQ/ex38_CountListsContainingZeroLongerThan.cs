dc8a760f11314614aa28d19a40fa2142535f9f1c
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-07-18 00:13:50 +0300

    ex 38 done

---
+++ CodingExercises/Section07_LINQ/ex38_CountListsContainingZeroLongerThan.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Coding.Exercise
+{
+	public class Exercise
+	{
+		public static int CountListsContainingZeroLongerThan(
+			int length,
+			List<List<int>> listsOfNumbers)
+			{
+				return listsOfNumbers.Count(list => list.Contains(0) &&
+					list.Count > length);
+			}
+	}
+}
