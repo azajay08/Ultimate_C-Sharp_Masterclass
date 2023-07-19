a1b115e6de1356d80fa8df5d715df28523690b56
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-07-19 22:30:29 +0300

    completed csv assignment

--- CsvDataAccess/NewSolution/FastTableData.cs
+++ CsvDataAccess/NewSolution/FastTableData.cs
@@ -4,18 +4,18 @@ namespace CsvDataAccess.NewSolution;
 
 public class FastTableData : ITableData
 {
-    private readonly List<FastRow> _rows;
-    public int RowCount => _rows.Count;
-    public IEnumerable<string> Columns { get; }
+	private readonly List<FastRow> _rows;
+	public int RowCount => _rows.Count;
+	public IEnumerable<string> Columns { get; }
 
-    public FastTableData(IEnumerable<string> columns, List<FastRow> rows)
-    {
-        _rows = rows;
-        Columns = columns;
-    }
+	public FastTableData(IEnumerable<string> columns, List<FastRow> rows)
+	{
+		_rows = rows;
+		Columns = columns;
+	}
 
-    public object GetValue(string columnName, int rowIndex)
-    {
-        return _rows[rowIndex].GetAtColumn(columnName);
-    }
+	public object GetValue(string columnName, int rowIndex)
+	{
+		return _rows[rowIndex].GetAtColumn(columnName);
+	}
 }
