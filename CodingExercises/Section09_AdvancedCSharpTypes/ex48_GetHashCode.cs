017a38550e5d275e68eff0314f28208d208e8353
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-07-31 22:54:39 +0300

    ex 48 done

---
+++ CodingExercises/Section09_AdvancedCSharpTypes/ex48_GetHashCode.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace Coding.Exercise
+{
+	public struct Time
+	{
+		public int Hour { get; }
+		public int Minute { get; }
+
+		public Time(int hour, int minute)
+		{
+			if (hour < 0 || hour > 23)
+			{
+				throw new ArgumentOutOfRangeException(
+					"Hour is out of range of 0-23");
+			}
+			if (minute < 0 || minute > 59)
+			{
+				throw new ArgumentOutOfRangeException(
+					"Minute is out of range of 0-59");
+			}
+			Hour = hour;
+			Minute = minute;
+		}
+
+		public override string ToString() =>
+			$"{Hour.ToString("00")}:{Minute.ToString("00")}";
+
+		public override bool Equals(object? obj)
+		{
+			return obj is Time other &&
+				other.Hour == Hour &&
+				other.Minute == Minute;
+		}
+		
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(Hour, Minute);
+		}
+	}
+}
