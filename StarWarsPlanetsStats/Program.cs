da86285d4080c955219c0783cbbe86657312d0d0
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-08-08 14:39:57 +0300

    added json property file class to DTO dir

--- StarWarsPlanetsStats/Program.cs
+++ StarWarsPlanetsStats/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using StarWarsPlanetsStats.ApiDataAccess;
 
+IApiDataReader apiDataReader = new ApiDataReader();
+var json = await apiDataReader.Read(
+	"https://swapi.dev/", "api/planets");
+
+var root = JsonSerializer.Deserialize<Root>(json);
+
+Console.WriteLine("Press any key to close.");
+Console.ReadKey();
