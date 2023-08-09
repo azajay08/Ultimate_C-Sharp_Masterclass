ac6b5b8f438c9de42546fb8b137abf552b9aeb78
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-08-09 19:56:18 +0300

    now printing the planets

--- StarWarsPlanetsStats/Program.cs
+++ StarWarsPlanetsStats/Program.cs
@@ -4,7 +4,9 @@ using StarWarsPlanetsStats.DTOs;
 
 try
 {
-	await new StarWarsPlanetsStatsApp().Run();
+	await new StarWarsPlanetsStatsApp(
+		new ApiDataReader(),
+		new MockStarWarsApiDataReader()).Run();
 }
 catch(Exception ex)
 {
@@ -17,13 +19,24 @@ Console.ReadKey();
 
 public class StarWarsPlanetsStatsApp
 {
+	private readonly IApiDataReader _apiDataReader;
+	private readonly IApiDataReader _backupApiDataReader;
+
+	public StarWarsPlanetsStatsApp(
+		IApiDataReader apiDataReader,
+		IApiDataReader backupApiDataReader)
+	{
+		_apiDataReader = apiDataReader;
+		_backupApiDataReader = backupApiDataReader;
+	}
+
 	public async Task Run()
 	{
-		string json = null;
+		string? json = null;
 
 		try
 		{
-			IApiDataReader apiDataReader = new ApiDataReader();
+			IApiDataReader apiDataReader = _apiDataReader;
 			json = await apiDataReader.Read(
 				"https://swapi.dev/", "api/planets");
 		}
@@ -35,12 +48,28 @@ public class StarWarsPlanetsStatsApp
 		}
 		if (json is null)
 		{
-			IApiDataReader apiDataReader = new MockStarWarsApiDataReader();
+			IApiDataReader apiDataReader = _backupApiDataReader;
 			json = await apiDataReader.Read(
 				"https://swapi.dev/", "api/planets");
 		}
 
 		var root = JsonSerializer.Deserialize<Root>(json);
+
+		var planets = ConvertToPlanets(root);
+
+		foreach (var p in planets)
+			Console.WriteLine(p);
+
+	}
+
+	private IEnumerable<Planet> ConvertToPlanets(Root? root)
+	{
+		if (root is null)
+		{
+			throw new ArgumentNullException(nameof(root));
+		}
+		return root.results.Select(
+			planetDto => (Planet)planetDto);
 	}
 }
 
