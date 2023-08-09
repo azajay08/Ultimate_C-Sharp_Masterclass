1579e69731d8e5e0df9818facd73982a7c6bd22d
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-08-09 16:03:48 +0300

    started app class

--- StarWarsPlanetsStats/Program.cs
+++ StarWarsPlanetsStats/Program.cs
@@ -1,11 +1,46 @@
 ﻿using System.Text.Json;
 using StarWarsPlanetsStats.ApiDataAccess;
+using StarWarsPlanetsStats.DTOs;
 
-IApiDataReader apiDataReader = new ApiDataReader();
-var json = await apiDataReader.Read(
-	"https://swapi.dev/", "api/planets");
-
-var root = JsonSerializer.Deserialize<Root>(json);
+try
+{
+	await new StarWarsPlanetsStatsApp().Run();
+}
+catch(Exception ex)
+{
+	Console.WriteLine("An error occured. " +
+		"Exception message: " + ex.Message);
+}
 
 Console.WriteLine("Press any key to close.");
 Console.ReadKey();
+
+public class StarWarsPlanetsStatsApp
+{
+	public async Task Run()
+	{
+		string json = null;
+
+		try
+		{
+			IApiDataReader apiDataReader = new ApiDataReader();
+			json = await apiDataReader.Read(
+				"https://swapi.dev/", "api/planets");
+		}
+		catch (HttpRequestException ex)
+		{
+			Console.WriteLine("API request was unsuccessful. " +
+				"Switching to mock data. " +
+				"Exception message: " + ex.Message);
+		}
+		if (json is null)
+		{
+			IApiDataReader apiDataReader = new MockStarWarsApiDataReader();
+			json = await apiDataReader.Read(
+				"https://swapi.dev/", "api/planets");
+		}
+
+		var root = JsonSerializer.Deserialize<Root>(json);
+	}
+}
+
