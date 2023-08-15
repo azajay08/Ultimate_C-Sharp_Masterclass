56e52be76820762c196329b3ff8b1b489e75de25
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-08-15 19:28:12 +0300

    ex 53 done

---
+++ CodingExercises/Section10_Collections/ex53_YieldExercise.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Coding.Exercise
+{
+	public class YieldExercise
+	{
+		public static IEnumerable<T> GetAllAfterLastNullReversed<T>(IList<T> input)
+		{
+			for (int i = input.Count - 1; i >= 0; i--)
+			{
+				if (input[i] == null)
+				{
+					yield break;
+				}
+				yield return input[i];
+			}
+		}
+	}
+}
