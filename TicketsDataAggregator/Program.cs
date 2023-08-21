743b496565c1dce44d6f774763ae7f1d4aec3993
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-08-21 11:05:21 +0300

    can succesfully read the pdf using pdf pig

--- TicketsDataAggregator/Program.cs
+++ TicketsDataAggregator/Program.cs
@@ -1,3 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
 
+const string TicketsFolder = "Tickets";
+
+try
+{
+	var ticketsAggregator = new TicketsAggregator(
+		TicketsFolder);
+
+	ticketsAggregator.Run();
+}
+catch (Exception ex)
+{
+	Console.WriteLine("An exception occurred. " +
+		"Exception message: " + ex.Message);
+}
+
+Console.WriteLine("Press any key to close.");
+Console.ReadKey();
+
+
+public class TicketsAggregator
+{
+	private readonly string _ticketsFolder;
+
+	public TicketsAggregator(
+		string ticketsFolder)
+	{
+		_ticketsFolder = ticketsFolder;
+	}
+
+	public void Run()
+	{
+		using (PdfDocument document = PdfDocument.Open(_ticketsFolder + "/Tickets1.pdf"))
+		{
+
+			// Page number starts from 1, not 0.
+			Page page = document.GetPage(1);
+			string text = page.Text;
+		}
+
+	}
+}
