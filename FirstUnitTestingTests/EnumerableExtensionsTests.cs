a64f9f3aedcbfee3915099bf1e2b932e640b90a8
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-08-22 16:11:53 +0300

    added more examples for basic unit tests

--- FirstUnitTestingTests/EnumerableExtensionsTests.cs
+++ FirstUnitTestingTests/EnumerableExtensionsTests.cs
@@ -9,7 +9,7 @@ namespace FirstUnitTestingTests;
 public class EnumerableExtensionsTests
 {
 	[Test]
-	public void SumOfEvenNumbers_ShouldReturnZero_EmptyCollection()
+	public void SumOfEvenNumbers_ShallReturnZero_EmptyCollection()
 	{
 		var input = Enumerable.Empty<int>();
 
@@ -19,7 +19,7 @@ public class EnumerableExtensionsTests
 	}
 
 	[Test]
-	public void SumOfEvenNumbers_ShouldReturnNonZeroResult_IfEvenNumbersArePresent()
+	public void SumOfEvenNumbers_ShallReturnNonZeroResult_IfEvenNumbersArePresent()
 	{
 		var input = new int[] {1, 3, 4, 2, 4, 5};
 
@@ -31,6 +31,45 @@ public class EnumerableExtensionsTests
 			$"the result shall be {expected} but it was {result}");
 		// The thrid argument to are equal is a message that will be triggered
 		// if the test fails
+	}
+
+	[TestCase(8)]
+	[TestCase(-8)]
+	[TestCase(6)]
+	[TestCase(0)]
+	public void SumOfEvenNumbers_ShallReturnValueOfTheOnlyItem_IfItsIsEven(int number)
+	{
+		var input = new int[] {number};
+
+		var result = input.SumOfEvenNumbers();
+
+		Assert.AreEqual(number, result);
+	}
+
+	[TestCase(8, 8)]
+	[TestCase(-8, -8)]
+	[TestCase(6, 6)]
+	[TestCase(0, 0)]
+	public void SumOfEvenNumbers_ShallReturnValueOfTheOnlyItem_IfItsIsEven_TwoParams(int number, int expected)
+	{
+		var input = new int[] { number };
+
+		var result = input.SumOfEvenNumbers();
+
+		Assert.AreEqual(expected, result);
+	}
+
+	[TestCase(7, 0)]
+	[TestCase(33, 0)]
+	[TestCase(1, 0)]
+	[TestCase(13, 0)]
+	public void SumOfEvenNumbers_ShallReturnZero_IfItsIsOdd_TwoParams(int number, int expected)
+	{
+		var input = new int[] { number };
+
+		var result = input.SumOfEvenNumbers();
+
+		Assert.AreEqual(expected, result);
 	}
 }
 
