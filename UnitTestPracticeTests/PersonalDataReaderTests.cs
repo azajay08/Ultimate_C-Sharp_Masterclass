43b184a7a5da187ae4f7192c6d0b306bcf7650f8
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-08-23 17:06:36 +0300

    starting last assignment

--- UnitTestPracticeTests/PersonalDataReaderTests.cs
+++ UnitTestPracticeTests/PersonalDataReaderTests.cs
@@ -7,13 +7,37 @@ namespace UnitTestPracticeTests;
 
 public class PersonalDataReaderTests
 {
+	private PersonalDataReader _cut; // Class Under Test
+	private Mock<IDatabaseConnection> _databaseConnectionMock;
+
+	[SetUp] // This acts sort of like a constructor
+	public void Setup()
+	{
+		_databaseConnectionMock = new Mock<IDatabaseConnection>();
+		_cut = new PersonalDataReader(
+			_databaseConnectionMock.Object);
+	}
+
 	[Test]
 	public void Read_ShallProduceResultWithDataOfPersonReadFromDataBase()
 	{
-		var databaseConnectionMock = new Mock<IDatabaseConnection>();
-		databaseConnectionMock
+		_databaseConnectionMock
 			.Setup(mock => mock.GetById(5))
 			.Returns(new Person(5, "John", "Smith"));
+
+		string result = _cut.Read(5);
+
+		Assert.AreEqual("(Id: 5) John Smith", result);
+
+		//var databaseConnectionMock = new Mock<IDatabaseConnection>();
+		//databaseConnectionMock
+		//	.Setup(mock => mock.GetById(5))
+		//	.Returns(new Person(5, "John", "Smith"));
+		//var personalDataReader = new PersonalDataReader(
+		//	databaseConnectionMock.Object);
+
+		//string result = personalDataReader.Read(5);
+		//Assert.AreEqual("(Id: 5) John Smith", result);
 
 		//databaseConnectionMock
 		//	.Setup(mock => mock.GetById(It.IsAny<int>()))
@@ -27,16 +51,12 @@ public class PersonalDataReaderTests
 		// We can use the Throw method to throw an exception instead of
 		// return a value
 
-		var personalDataReader = new PersonalDataReader(
-			databaseConnectionMock.Object);
-
-		string result = personalDataReader.Read(5);
+		//var personalDataReader = new PersonalDataReader(
+		//	databaseConnectionMock.Object);
 
 		//databaseConnectionMock.Reset();
 		// This would reset the mock so we can assign it again, but it would
 		// make the test too complicated
-
-		Assert.AreEqual("(Id: 5) John Smith", result);
 	}
 
 	// Testing a method that is a void
@@ -49,14 +69,19 @@ public class PersonalDataReaderTests
 		var personToBeSaved = new Person(10, "Jane", "Miller");
 		persoanlDataReader.Save(personToBeSaved);
 
-		//databaseConnectionMock.Verify(mock => mock.Write(
-		//	personToBeSaved.Id, personToBeSaved));
+		databaseConnectionMock.Verify(mock => mock.Write(
+			personToBeSaved.Id, personToBeSaved));
 
 		// Checking if if the Write method is only called once..
-		// can also check Never 
-		databaseConnectionMock.Verify(mock => mock.Write(
-			personToBeSaved.Id, personToBeSaved),
-			Times.Once());
+		// can also check Never(), Exactly(n), AtLeast(n), AtMost(n), Between(n, n) 
+		//databaseConnectionMock.Verify(mock => mock.Write(
+		//	personToBeSaved.Id, personToBeSaved),
+		//	Times.Once());
+
+		// If we want to just check if the Write method has been called and the
+		// arguments don't matter, we can it with the IsAny method..
+		//databaseConnectionMock.Verify(mock => mock.Write(
+		//	It.IsAny<int>(), It.IsAny<Person>()));
 	}
 }
 
